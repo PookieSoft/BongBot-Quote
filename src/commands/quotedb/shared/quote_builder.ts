fbf57952b0c2b785437b19ef6c11a0b9eb69e109
Mirasii <meatheat_100mean@hotmail.co.uk>
2026-04-11 17:11:18 +0100

    implementation

---
+++ src/commands/quotedb/shared/quote_builder.ts
@@ -0,0 +1,31 @@
+import { EmbedBuilder, Colors } from 'discord.js';
+import { ExtendedClient } from '@pookiesoft/bongbot-core';
+
+export default class QuoteBuilder {
+    private embed: EmbedBuilder;
+    constructor() {
+        this.embed = new EmbedBuilder();
+        return this;
+    }
+
+    setTitle(title: string): QuoteBuilder {
+        this.embed.setTitle(`📜 ${title}`);
+        return this;
+    }
+
+    addQuotes(quotes: { quote: string, author: string }[]): QuoteBuilder {
+        this.embed.addFields(quotes.map((quote) => ({
+            name: `*"${quote.quote}"*`,
+            value: `🪶 - ${quote.author}`,
+            inline: false
+        })));
+        return this;
+    }
+
+    build(client: ExtendedClient): { embeds: [EmbedBuilder] } {
+        this.embed.setFooter({ text: `BongBot • Quotes from quotes.elmu.dev`, iconURL: client.user?.displayAvatarURL() });
+        this.embed.setTimestamp();
+        this.embed.setColor(Colors.Purple);
+        return { embeds: [this.embed] };
+    }
+}
---
+++ src/commands/quotedb/shared/quotedb_api.ts
@@ -0,0 +1,53 @@
+import { Caller, ExtendedClient, buildError } from '@pookiesoft/bongbot-core';
+import config from '@pookiesoft/bongbot-core';
+import { ChatInputCommandInteraction } from 'discord.js';
+import QuoteBuilder from './quote_builder.js';
+const api = config.apis.quotedb;
+
+export class QuoteDBAPI {
+    readonly _caller: Caller;
+
+    constructor(caller: Caller) {
+        this._caller = caller;
+    }
+
+    async getQuotes(client: ExtendedClient, interaction: ChatInputCommandInteraction, amount: number, route: string) {
+        const response = await this._caller.get(
+            api.url,
+            `/api/v1/quotes/${route}/user/${api.user_id}`,
+            `max_quotes=${amount}`,
+            { 'Content-Type': 'application/json', 'Authorization': `Bearer ${api.apikey}` },
+        );
+        if (response?.quotes?.length === 0) {
+            return await buildError(interaction, new Error("No quotes found."));
+        }
+        return new QuoteBuilder()
+            .setTitle(`${route === 'random' ? 'Random Quote' : 'Recent Quote'}${response.quotes.length > 1 ? `s` : ''}`)
+            .addQuotes(response.quotes)
+            .build(client);
+    }
+
+    async createQuote(content: NewQuote, client: ExtendedClient) {
+        const response = await this._caller.post(
+            api.url,
+            '/api/v1/quotes',
+            { 'Content-Type': 'application/json', 'Authorization': `Bearer ${api.apikey}` },
+            {
+                quote: content.quote,
+                author: content.author,
+                user_id: api.user_id,
+                date: new Date().toLocaleString()
+            }
+        );
+        return new QuoteBuilder()
+            .setTitle(`New Quote Created`)
+            .addQuotes([response.quote])
+            .build(client);
+    }
+
+}
+
+interface NewQuote {
+    quote: string;
+    author: string;
+}
